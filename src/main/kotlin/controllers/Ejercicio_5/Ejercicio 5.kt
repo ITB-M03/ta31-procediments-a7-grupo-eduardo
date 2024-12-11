7f56e64e8588e176a810b353e212d93627d7c65a
denisitoow <denis.coello.7e8@itb.cat>
2024-12-11 08:38:46 +0100

    menu2

--- src/main/kotlin/controllers/Ejercicio_5/Ejercicio 5.kt
+++ src/main/kotlin/controllers/Ejercicio_5/Ejercicio 5.kt
@@ -1,2 +1,17 @@
 package controllers.Ejercicio_5
 
+
+
+
+fun menu2(eleccion : Int){
+    when(eleccion){
+        1 -> push() ; menu()
+        2 -> pop() ; menu()
+        3 -> mostrar() ; menu()
+        4 -> salir()
+        else -> {
+            println("Opcion invalida")
+            menu(eleccion)
+        }
+    }
+}
