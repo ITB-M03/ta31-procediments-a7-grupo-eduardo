28c79752ee6545168e94b0f7bdeeb14b6058852b
IvanTorres02 <ivan.torres.7e8@itb.cat>
2024-12-11 08:45:00 +0100

    primer push

--- src/main/kotlin/controllers/Ejercicio_5/Ejercicio 5.kt
+++ src/main/kotlin/controllers/Ejercicio_5/Ejercicio 5.kt
@@ -1,14 +1,41 @@
 package controllers.Ejercicio_5
 
+import utilities.iniciarScan
+import java.awt.Menu
+import java.util.Scanner
+
+fun main() {
+    val scan = iniciarScan()
+    menu(scan)
+    menu2(menu, scan)
 
 
+}
 
-fun menu2(eleccion : Int){
-    when(eleccion){
-        1 -> push() ; menu()
-        2 -> pop() ; menu()
-        3 -> mostrar() ; menu()
-        4 -> salir()
+fun menu(scan: Scanner): Int {
+    var resultat: Int
+    println(
+        """
+        1-Afegir un numero (Push)
+        2-Treure un numero (Pop)
+        3-Mostrar el contingut de la pila
+        4-Sortir"""
+    )
+    resultat = scan.nextInt()
+    return resultat
+}
+
+fun push(scan: Scanner) {
+    var numero: Int = scan.nextInt()
+}
+
+
+fun menu2(menu: Int, scan: Scanner) {
+    when (menu) {
+        1 -> push(scan); menu(scan)
+        2 -> pop(); menu(scan)
+        3-> mostrar(); menu(scan)
+        4-> salir()
         else -> {
             println("Opcion invalida")
             menu(eleccion)
